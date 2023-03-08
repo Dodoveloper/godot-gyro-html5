0210d059e91ebf18c340f114d47e26b27da071d4
Dodoveloper <davide.cor94@gmail.com>
2023-03-08 11:56:55 +0100

    fix: set camera limits correctly, fixed HUD

--- main.gd
+++ main.gd
@@ -1,14 +1,30 @@
 extends Node2D
 
 
+const TILT_TRESHOLD := 5.0
+const CAMERA_STEP := 3  # pixels
+
+onready var camera := $Camera2D as Camera2D
 onready var sensor_component := $SensorComponent as SensorComponent
-onready var os_label := $Control/OS as Label
-onready var gyro_label := $Control/Gyroscope as Label
+onready var os_label := $"%OSLabel" as Label
+onready var gyro_label := $"%GyroscopeLabel" as Label
 # iOS only
-onready var enable_gyro_btn := $Control/EnableGyro as CheckButton
+onready var enable_gyro_btn := $"%EnableGyroBtn" as CheckButton
+onready var farthest_layer := $ParallaxBackground/ParallaxLayer1 as ParallaxLayer
+onready var default_camera_pos := camera.position.x
 
 
 func _ready() -> void:
+	# dynamically set camera limits based on the farthest parallax layer
+	var screen_size_x: int = ProjectSettings.get("display/window/size/width")
+	var background_extents := (farthest_layer.get_node("BackgroundFlat") as Sprite).\
+			texture.get_size()
+	# get the distance between one edge of the background and one of the screen size,
+	# then scale it based on the background's parallax layer
+	var scaled_delta_x := round(abs(background_extents.x - screen_size_x) / 2\
+			/ farthest_layer.motion_scale.x)
+	camera.limit_left = int(-scaled_delta_x)
+	camera.limit_right = int(scaled_delta_x - screen_size_x)
 	# update UI
 	enable_gyro_btn.hide()
 	os_label.text = "OS: %s" % sensor_component.os_string
@@ -24,10 +40,16 @@ func _on_EnableGyro_toggled(button_pressed: bool) -> void:
 
 # Here is where the sensor data is received, in case of success
 func _on_SensorComponent_gyroscope_triggered(coords: Vector3) -> void:
+	# display text on the UI
 	var text := "(%.2f, %.2f, %.2f)" % [coords.x, coords.y, coords.z]
 	gyro_label.text = "Gyroscope: %s" % text
+	# camera-moving logic
+	var relative_scroll: float = coords.y if abs(coords.y) > TILT_TRESHOLD else 0.0
+	camera.position.x += CAMERA_STEP * (relative_scroll / 2.5)
+#	camera_pos.x = lerp(camera_pos.x, camera_pos.x + relative_scroll, 0.5)
 
 
 # Updates the checkbox button based on the outcome of the request
 func _on_SensorComponent_ios_permission_requested(value: bool) -> void:
 	enable_gyro_btn.pressed = value
+	enable_gyro_btn.disabled = true
